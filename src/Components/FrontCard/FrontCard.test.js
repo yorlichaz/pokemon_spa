a8930e33330922e4c2b1858b74957675cd7f7247
ilroy <yorli0114@gmail.com>
2020-06-26 09:02:57 -0400

    added test for FrontCard

--- src/Components/FrontCard/FrontCard.test.js
+++ src/Components/FrontCard/FrontCard.test.js
@@ -1,14 +1,32 @@
-import { unmountComponentAtNode } from 'react-dom';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import React from "react";
+import FrontCard from "./FrontCard";
 
 let container = null;
-beforeEach(()=>{
-    container.document.createElement("div");
-    document.body.appendChild(container);
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
 
-afterEach(()=>{
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
 });
 
+it("renders with pokemon name and picture", () => {
+  act(() => {
+    render(
+      <FrontCard
+        image={
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+        }
+        name={"Ditto"}
+      />,
+      container
+    );
+  });
+  expect(container.querySelector("figcaption").textContent).toBe("Ditto");
+  expect(container.querySelector("img")["src"]).toBe( "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png");
+});
