7bb623567e20dba8031d5e270806f22d16748f58
Yorli <yorli0114@gmail.com>
2020-08-25 12:57:31 -0400

    Updates

---
+++ precache-manifest.a9d65db387ed026d296dc9eae2f9a371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e196c3d56651e23faebf7daf43204898",
+    "url": "/pokemon_spa/index.html"
+  },
+  {
+    "revision": "2465029e6729245d76ae",
+    "url": "/pokemon_spa/static/css/main.9f930f05.chunk.css"
+  },
+  {
+    "revision": "56d4bdeb98e91ab4317a",
+    "url": "/pokemon_spa/static/js/2.26b9bcca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon_spa/static/js/2.26b9bcca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2465029e6729245d76ae",
+    "url": "/pokemon_spa/static/js/main.948d2e3e.chunk.js"
+  },
+  {
+    "revision": "beadf937b496dc961412",
+    "url": "/pokemon_spa/static/js/runtime-main.a872fd6f.js"
+  }
+]);
